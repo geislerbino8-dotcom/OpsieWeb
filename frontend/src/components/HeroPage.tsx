92fbb4843ef1bd872ca12ebe5489a08dde2dc05a
Tecsonology <139662416+Tecsonology@users.noreply.github.com>
2026-03-06 15:48:55 +0800

    Contact us style fixed

--- frontend/src/components/HeroPage.tsx
+++ frontend/src/components/HeroPage.tsx
@@ -1,6 +1,4 @@
-import { SplitText } from 'gsap/all';
 import LogoOnly from '../assets/icons/opsie_logo_only.png';
-import github from '../assets/visuals/github.jpg'
 
 type HeroPageProps = {
   heroText?: string;
@@ -8,11 +6,6 @@ type HeroPageProps = {
 };
 
 function HeroPage({ heroText, bgImage }: HeroPageProps) {
-
-  const handleAnimationComplete = () => {
-    console.log('All letters have animated!');
-  };
-
   return (
     <div
       className="w-full h-screen  relative flex flex-col justify-end"
@@ -22,31 +15,12 @@ function HeroPage({ heroText, bgImage }: HeroPageProps) {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundBlendMode: 'darken'
-          
       }}
-
     >
       {/* Logo Overlay */}
       <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <img src={LogoOnly} alt="Logo" className="w-72 md:w-96 opacity-70" />
       </div>
-
-      <SplitText
-        text="Hello, you!"
-        className="text-2xl font-semibold text-center"
-        delay={50}
-        duration={1.25}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        textAlign="center"
-        onLetterAnimationComplete={handleAnimationComplete}
-        showCallback
-      />
-
 
       {/* Hero Text & Button */}
       <div className="relative z-10 flex flex-col items-center text-center px-4 md:px-8 mb-16 space-y-6">
@@ -71,7 +45,6 @@ function HeroPage({ heroText, bgImage }: HeroPageProps) {
           GET STARTED
         </button>
       </div>
-
     </div>
   );
 }
